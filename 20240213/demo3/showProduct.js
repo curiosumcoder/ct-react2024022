8e8c6f85d203b4e00066fcbfe160e1f7f23266d2
Gilberto Bermúdez Garro <gbermude@outlook.com>
2024-02-13 22:20:53 -0600

    Update

--- 20240213/demo3/showProduct.js
+++ 20240213/demo3/showProduct.js
@@ -2,5 +2,5 @@ const showProduct = (id, name, price) => {
     console.info('Prueba de TypeScript!' + new Date().toLocaleTimeString());
     console.table([id, name, price]);
 };
-showProduct('1', 'Laptop', 1500);
+showProduct(1, 'Laptop', 1500);
 export default showProduct;
